cf852bddb8f79245d8f6e0ed123fc95748cf11f0
Michel Samir <michelsamir@gameit.ai>
2023-04-07 16:23:10 +0200

    add other ports

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const edgeMultiplay = require('edge-multiplay')();
 
 console.log(process.env.PORT);
+console.log(process.env.PORT2);
+console.log(process.env.PORT3);
 edgeMultiplay.wsServer.on('newConnection', (path, connection)=>{
    edgeMultiplay.addLobby(connection)
 })
