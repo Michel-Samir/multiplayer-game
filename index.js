c65b21484af50ad7bcf5d006a99029b9021767d8
Michel Samir <michelsamir@gameit.ai>
2023-04-13 09:36:37 +0200

    app

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 const edgeMultiplay = require('edge-multiplay')();
 
-console.log(process.env.PORT);
-console.log(process.env.PORT2);
-console.log(process.env.PORT3);
 edgeMultiplay.wsServer.on('newConnection', (path, connection)=>{
    edgeMultiplay.addLobby(connection)
 })
